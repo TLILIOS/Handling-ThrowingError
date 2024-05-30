1aa5562f9fe80898a3cf31d01e165ac9f9fc1466
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-30 17:47:33 +0200

    Handling Error

---
+++ Handling&ThrowingError/Handling&ThrowingError/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Handling&ThrowingError
+//
+//  Created by HTLILI on 30/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
